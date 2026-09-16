c53fa21a46c331d710229cdbec251dd4d1171b61
alexanderbaraxanov <alexander.baraxanov@gmail.com>
2026-09-16 17:24:48 +0300

    описаны тесты для функций presentation

--- src/__test__/presentation.test.ts
+++ src/__test__/presentation.test.ts
@@ -1,1 +1,42 @@
 import { describe, it, expect } from 'vitest';
+import {
+    createPresentation,
+    updatePresentationName,
+    savePresentation,
+    loadPresentation,
+} from '../functions/presentation.ts';
+import { generateId } from '../functions/objects.ts';
+
+it('should create presentation with defaul slide', () => {
+    const presentationId = generateId();
+    const slideId = generateId();
+    const presentation = createPresentation('my_presentation', slideId, presentationId);
+
+    expect(presentation.id).toBe(presentationId);
+    expect(presentation.name).toEqual('my_presentation');
+    expect(presentation.slides.length).toBe(1);
+})
+
+it('should rename presentation', () => {
+    const presentationId = generateId();
+    const slideId = generateId();
+    const presentation = createPresentation('my_presentation', slideId, presentationId); 
+
+    const updatedPresentation = updatePresentationName(presentation, 'new_presentation');
+    
+    expect(updatedPresentation.id).toBe(presentation.id);
+    expect(updatedPresentation.name).toEqual('new_presentation');
+    expect(updatedPresentation.slides).toBe(presentation.slides);
+})
+
+it('should correctly save and load presentation', () => {
+    const presentationId = generateId();
+    const slideId = generateId();
+    const presentation = createPresentation('my_presentation', slideId, presentationId);
+    
+    const savedPresentation = savePresentation(presentation);
+    const loadedPresentation = loadPresentation(savedPresentation);
+
+    expect(loadedPresentation).toEqual(presentation);
+    expect(loadedPresentation.slides).not.toBe(presentation.slides);
+})
