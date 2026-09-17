30d01c611a07ad13ddd45d19458c2af9e4b3fe0d
alexanderbaraxanov <alexander.baraxanov@gmail.com>
2026-09-17 09:16:50 +0300

    изменена работа функции createPresentation

--- src/functions/presentation.ts
+++ src/functions/presentation.ts
@@ -1,12 +1,10 @@
 import type { Presentation } from '../types/presentation.ts';
-import { createDefaultSlide } from './slide.js';
 
-function createPresentation(name: string, idSlide: string ,id: string): Presentation {
-    const defaultSlide = createDefaultSlide('Слайд 1', idSlide); 
+function createPresentation(name: string, id: string): Presentation {
     return {
         id: id,
         name: name,
-        slides: [defaultSlide],
+        slides: [],
     }
 }
 
